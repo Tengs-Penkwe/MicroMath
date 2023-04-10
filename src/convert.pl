9abea3da127c0728cd31af09b45a4e45a6b4a36f
Tengs Penkwe <tengspenkwe@gmail.com>
2023-04-10 00:28:05 -0700

    convert AST to atoms, and many tests

---
+++ src/convert.pl
@@ -0,0 +1,56 @@
+:- module(convert, [ast_to_string/2]).
+
+ast_to_string(num(N), S) :- atom_number(S, N).
+ast_to_string(vrb(X), X).
+
+ast_to_string(add(E1, E2), S) :- ast_to_string_binop(E1, E2, add, S).
+ast_to_string(sub(E1, E2), S) :- ast_to_string_binop(E1, E2, sub, S).
+ast_to_string(mul(E1, E2), S) :- ast_to_string_binop(E1, E2, mul, S).
+ast_to_string(dvd(E1, E2), S) :- ast_to_string_binop(E1, E2, dvd, S).
+ast_to_string(pow(E1, E2), S) :- ast_to_string_binop(E1, E2, pow, S).
+
+ast_to_string(sin(E), S) :- ast_to_string(E, S1), atomic_list_concat(['sin(', S1, ')'], S).
+ast_to_string(cos(E), S) :- ast_to_string(E, S1), atomic_list_concat(['cos(', S1, ')'], S).
+ast_to_string(exp(E), S) :- ast_to_string(E, S1), atomic_list_concat(['exp(', S1, ')'], S).
+ast_to_string(ln(E), S)  :- ast_to_string(E, S1), atomic_list_concat(['ln(', S1, ')'], S).
+
+
+ast_to_string(diff(E, X), S) :-
+    ast_to_string(E, S1),
+    ast_to_string(X, S2),
+    atomic_list_concat(['D', S2, '(', S1, ')'], S).
+
+ast_to_string(integ(E, X), S) :-
+    ast_to_string(E, S1),
+    ast_to_string(X, S2),
+    atomic_list_concat(['∫', '(', S1, ')', S2], S).
+
+% Helper predicate for binary operations
+ast_to_string_binop(E1, E2, Op, S) :-
+    ast_to_string(E1, S1),
+    ast_to_string(E2, S2),
+    operator_string(Op, Opstr),
+    (needs_paren(E1, Op) -> (P1 = '(', P2 = ')') ; (P1 = '', P2 = '')),
+    (needs_paren(E2, Op) -> (P3 = '(', P4 = ')') ; (P3 = '', P4 = '')),
+    atomic_list_concat([P1, S1, P2, Opstr, P3, S2, P4], S).
+
+operator_string(add, '+').
+operator_string(sub, '-').
+operator_string(mul, "*").
+operator_string(dvd, "/").
+operator_string(pow, "^"). 
+
+
+needs_paren(add(_, _), Op) :- member(Op, [sub, mul, dvd, pow]).
+needs_paren(sub(_, _), Op) :- member(Op, [mul, dvd, pow]).
+needs_paren(mul(_, _), Op) :- member(Op, [pow]).
+needs_paren(dvd(_, _), Op) :- member(Op, [pow]).
+needs_paren(_, _) :- false.
+
+% needs_paren(add(_, _), sub) :- !.
+% needs_paren(mul(_, _), dvd) :- !.
+% needs_paren(mul(_, _), mul) :- !.
+% needs_paren(dvd(_, _), mul) :- !.
+% needs_paren(dvd(_, _), dvd) :- !.
+% needs_paren(_, _).
+
---
+++ tests/convert_ts.pl
@@ -0,0 +1,143 @@
+:- use_module('../src/convert.pl').
+
+:- begin_tests(ast_to_string).
+
+test(num) :-
+    ast_to_string(num(5), '5').
+
+test(numbers) :-
+    ast_to_string(num(42), '42').
+
+test(variable) :-
+    ast_to_string(vrb(x), x).
+
+test(add) :-
+    ast_to_string(add(num(1), num(2)), '1+2').
+
+test(sub) :-
+    ast_to_string(sub(num(1), num(2)), '1-2').
+
+test(mul) :-
+    ast_to_string(mul(num(1), num(2)), '1*2').
+
+test(dvd) :-
+    ast_to_string(dvd(num(1), num(2)), '1/2').
+
+test(pow) :-
+    ast_to_string(pow(num(1), num(2)), '1^2').
+
+test(sin) :-
+    ast_to_string(sin(num(1)), 'sin(1)').
+
+test(cos) :-
+    ast_to_string(cos(num(1)), 'cos(1)').
+
+test(exp) :-
+    ast_to_string(exp(num(1)), 'exp(1)').
+
+test(ln) :-
+    ast_to_string(ln(num(1)), 'ln(1)').
+
+test(diff) :-
+    ast_to_string(diff(num(1), vrb(x)), 'Dx(1)').
+
+test(integ) :-
+    ast_to_string(integ(num(1), vrb(x)), '∫(1)x').
+
+test(nested) :-
+    ast_to_string(add(num(1), mul(num(2), num(3))), '1+2*3').
+
+:- end_tests(ast_to_string).
+
+:- begin_tests(parentheses).
+
+test(simple_add) :-
+    ast_to_string(add(num(1), num(2)), '1+2').
+
+test(simple_sub) :-
+    ast_to_string(sub(num(1), num(2)), '1-2').
+
+test(simple_mul) :-
+    ast_to_string(mul(num(1), num(2)), '1*2').
+
+test(simple_dvd) :-
+    ast_to_string(dvd(num(1), num(2)), '1/2').
+
+test(add_mul) :-
+    ast_to_string(add(num(1), mul(num(2), num(3))), '1+2*3').
+
+test(add_dvd) :-
+    ast_to_string(add(num(1), dvd(num(2), num(3))), '1+2/3').
+
+test(mul_dvd) :-
+    ast_to_string(mul(num(1), dvd(num(2), num(3))), '1*2/3').
+
+test(sub_dvd) :-
+    ast_to_string(sub(num(1), dvd(num(2), num(3))), '1-2/3').
+
+test(nested_add_mul) :-
+    ast_to_string(add(num(1), add(mul(num(2), num(3)), num(4))), '1+2*3+4').
+
+test(nested_sub_mul) :-
+    ast_to_string(sub(num(1), sub(mul(num(2), num(3)), num(4))), '1-(2*3-4)').
+
+test(nested_add_dvd) :-
+    ast_to_string(add(num(1), add(dvd(num(2), num(3)), num(4))), '1+2/3+4').
+
+test(nested_sub_dvd) :-
+    ast_to_string(sub(num(1), sub(dvd(num(2), num(3)), num(4))), '1-(2/3-4)').
+
+test(nested_mul_dvd) :-
+    ast_to_string(mul(num(1), mul(dvd(num(2), num(3)), num(4))), '1*2/3*4').
+
+test(nested_add_mul_dvd) :-
+    ast_to_string(add(num(1), mul(dvd(num(2), num(3)), num(4))), '1+2/3*4').
+
+test(nested_sub_mul_dvd) :-
+    ast_to_string(sub(num(1), mul(dvd(num(2), num(3)), num(4))), '1-2/3*4').
+
+test(nested_dvd_dvd) :-
+    ast_to_string(dvd(num(1), dvd(num(2), num(3))), '1/(2/3)').
+
+test(nested_add_dvd_dvd) :-
+    ast_to_string(add(num(1), dvd(num(2), num(3))), '1+2/3').
+
+test(nested_sub_dvd_dvd) :-
+    ast_to_string(sub(num(1), dvd(num(2), num(3))), '1-2/3').
+
+test(nested_dvd_mul_dvd) :-
+    ast_to_string(dvd(num(1), mul(dvd(num(2), num(3)), num(4))), '1/(2/3*4)').
+
+:- end_tests(parentheses).
+
+:- begin_tests(ast_to_string_complex).
+
+test(nested_expression_5) :-
+    ast_to_string(add(mul(num(2), pow(vrb(x), num(2))), mul(num(3), vrb(x))), '2*x^2+3*x').
+
+test(nested_expression_6) :-
+    ast_to_string(dvd(mul(num(4), pow(vrb(x), num(2))), add(num(2), vrb(x))), '4*x^2/(2+x)').
+
+test(nested_expression_7) :-
+    ast_to_string(mul(sin(add(vrb(x), num(1))), cos(sub(vrb(x), num(1)))), 'sin(x+1)*cos(x-1)').
+
+test(nested_expression_8) :-
+    ast_to_string(exp(mul(num(2), add(vrb(x), num(3)))), 'exp(2*(x+3))').
+
+test(nested_expression_9) :-
+    ast_to_string(pow(dvd(num(1), add(vrb(x), num(1))), num(3)), '(1/(x+1))^3').
+
+test(nested_expression_10) :-
+    ast_to_string(diff(add(pow(vrb(x), num(2)), mul(num(3), vrb(x))), vrb(x)), 'Dx(x^2+3*x)').
+
+test(nested_expression_11) :-
+    ast_to_string(integ(mul(sin(vrb(x)), cos(vrb(x))), vrb(x)), '∫(sin(x)*cos(x))x').
+
+test(nested_expression_12) :-
+    ast_to_string(diff(exp(mul(vrb(x), pow(vrb(x), num(2)))), vrb(x)), 'Dx(exp(x*x^2))').
+
+test(nested_expression_13) :-
+    ast_to_string(integ(add(mul(num(2), pow(vrb(x), num(2))), mul(num(3), vrb(x))), vrb(x)), '∫(2*x^2+3*x)x').
+
+:- end_tests(ast_to_string_complex).
+
