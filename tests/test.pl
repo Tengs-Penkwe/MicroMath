427a278b6e424e4652ef21913d72a94369c41a96
Tengs Penkwe <tengspenkwe@gmail.com>
2023-04-10 22:36:19 -0700

    more tests

--- tests/test.pl
+++ tests/test.pl
@@ -17,6 +17,51 @@ test(simplify_division) :-
 test(simplify_exponentiation) :-
     micro_math("x^1", "x").
 
+test(simplify_add_zero) :-
+    micro_math("0+x", "x").
+
+% test(simplify_sub_zero) :-
+%     micro_math("0-x", "-x").
+
+test(simplify_mul_zero) :-
+    micro_math("x*0", "0").
+
+test(simplify_mul_zero_2) :-
+    micro_math("0*x", "0").
+
+test(simplify_div_zero) :-
+    micro_math("0/x", "0").
+
+test(simplify_exp_zero) :-
+    micro_math("x^0", "1").
+
+% test(simplify_neg_exp) :-
+%     micro_math("x^(-1)", "1/x").
+
+% test(simplify_double_neg) :-
+%     micro_math("(-x)^2", "x^2").
+
+% test(simplify_trig_identity) :-
+%     micro_math("sin(x)^2+cos(x)^2", "1").
+
+% test(simplify_exp_add) :-
+%     micro_math("exp(x)*exp(y)", "exp(x+y)").
+
+test(simplify_nested_trig) :-
+    micro_math("sin(cos(sin(0)))", "sin(1)").
+
+test(simplify_nested_pow) :-
+    micro_math("(x^2)^3", "x^6").
+
+test(simplify_nested_mul) :-
+    micro_math("2*(3*x)", "6*x").
+
+test(simplify_nested_div) :-
+    micro_math("(x/2)/4", "x/8").
+
+test(simplify_mixed_terms) :-
+    micro_math("x^2+2*x*y+y^2", "x^2+2*x*y+y^2").
+
 test(simplify_polynomial) :-
     micro_math("x^2+2*x+1", "x^2+2*x+1").
 
