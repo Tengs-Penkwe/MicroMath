542394ea3cebde0520993133455a3d4e268aa8dd
Tengs Penkwe <tengspenkwe@gmail.com>
2023-04-10 22:22:16 -0700

    some rule to simplify cos, sin, and exp

--- src/simp.pl
+++ src/simp.pl
@@ -1,4 +1,6 @@
 :- module(simp, [simplify/2]).
+% ALARM: we may need neg()
+:- discontiguous simplify_operation/4.
 
 % Base case: numbers and single variables are already simplified.
 simplify(num(N), num(N)).
@@ -11,21 +13,46 @@ simplify(mul(E1, E2), R) :- simplify_operation(E1, E2, mul, R).
 simplify(dvd(E1, E2), R) :- simplify_operation(E1, E2, dvd, R).
 simplify(pow(E1, E2), R) :- simplify_operation(E1, E2, pow, R).
 
+%
 simplify(integ(Expr, Var), R) :- simplify_operation(Expr, integ, Var, R).
 simplify(diff(Expr, Var), R)  :- simplify_operation(Expr, diff, Var, R).
 
+% Simplify cos, sin, and exp
+simplify(cos(Expr), R) :- simplify_trig(Expr, cos, R).
+simplify(sin(Expr), R) :- simplify_trig(Expr, sin, R).
+simplify(exp(Expr), R) :- simplify_exp(Expr, R).
+
+% Simplify the inner expression for cos and sin, then apply specific rules
+simplify_trig(Expr, Trig, R) :-
+    simplify(Expr, SimplifiedExpr),
+    (SimplifiedExpr = num(0) ->
+        (Trig = cos -> R = num(1); R = num(0))
+    ;
+        R =.. [Trig, SimplifiedExpr]
+    ).
+
+% Simplify the inner expression for exp, then apply specific rules
+simplify_exp(Expr, R) :-
+    simplify(Expr, SimplifiedExpr),
+    (SimplifiedExpr = num(0) ->
+        R = num(1)
+    ;
+        R = exp(SimplifiedExpr)
+    ).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Rules for Arithmetic Operation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  simplify_operation(E1, E2, Op, R) :-
-    simplify(E1, S1),
-    simplify(E2, S2),
-    (S1 = num(N1), S2 = num(N2) ->
-      arithmetic_op(Op, N1, N2, N3),
-      R = num(N3)
-    ;
-      R =.. [Op, S1, S2]
-    ).
+simplify_operation(E1, E2, Op, R) :-
+  simplify(E1, S1),
+  simplify(E2, S2),
+  (S1 = num(N1), S2 = num(N2) ->
+    arithmetic_op(Op, N1, N2, N3),
+    R = num(N3), !
+  ;
+    operation_expr(Op, S1, S2, S3),
+    R = S3, !
+    % R =.. [Op, S1, S2]
+  ).
 
 arithmetic_op(add, N1, N2, N3) :- N3 is N1 + N2.
 arithmetic_op(sub, N1, N2, N3) :- N3 is N1 - N2.
@@ -33,10 +60,89 @@ arithmetic_op(mul, N1, N2, N3) :- N3 is N1 * N2.
 arithmetic_op(dvd, N1, N2, N3) :- N3 is N1 / N2.
 arithmetic_op(pow, N1, N2, N3) :- N3 is N1 ^ N2.
 
-% operation_expr(+, S1, S2, add(S1, S2)).
-% operation_expr(-, S1, S2, sub(S1, S2)).
-% operation_expr(*, S1, S2, mul(S1, S2)).
-% operation_expr(/, S1, S2, dvd(S1, S2)).
+operation_expr(add, S1, S2, S3) :-
+  (S1 = num(0) ->
+    S3 = S2, !
+  ;S2 = num(0) ->
+    S3 = S1, !
+  ;
+    S3 = add(S1, S2)
+  ).
+operation_expr(sub, S1, S2, S3) :-
+  (
+    % S1 = num(0) ->
+    % S3 = S2
+    %   ALARM, TODO: add neg to AST
+  S2 = num(0) ->
+    S3 = S1, !
+  ;
+    S3 = sub(S1, S2)
+  ).
+operation_expr(mul, S1, S2, S3) :-
+  (S1 = num(0) ->
+      S3 = num(0), !
+  ;S1 = num(1) ->
+      S3 = S2, !
+  ;S2 = num(0) ->
+      S3 = num(0), !
+  ;S2 = num(1) ->
+      S3 = S1, !
+  ;S1 = vrb(X), S2 = vrb(X) ->
+      S3 = pow(vrb(X), num(2))
+  ;
+  (S1 = vrb(X), S2 = pow(vrb(X), E) -> 
+      simplify(add(E, num(1)), SS)
+  ;S1 = pow(vrb(X), E), S2 = vrb(X) -> 
+      simplify(add(E, num(1)), SS)
+  ),
+      S3 = pow(vrb(X), SS)
+  ;S1 = pow(vrb(X), E1), S2 = pow(vrb(X), E2) ->
+      simplify(add(E1, E2), SS),
+      S3 = pow(vrb(X), SS)
+  ;
+  S3 = mul(S1, S2)
+  ).
+operation_expr(dvd, S1, S2, S3) :-
+  (
+    S1 = num(0) ->
+    S3 = num(0), !
+    %   ALARM, TODO: divid zero error
+  % S2 = num(0) ->
+  %   S3 = S1
+  ;S2 = num(1) ->
+      S3 = S1, !
+  ;S1 = vrb(X), S2 = vrb(X) ->
+      S3 = num(1), !
+  ;
+  (S1 = vrb(X), S2 = pow(vrb(X), E) -> 
+      simplify(sub(num(1), E), SS)
+  ;S1 = pow(vrb(X), E), S2 = vrb(X) -> 
+      simplify(sub(E, num(1)), SS)
+  ),
+      S3 = pow(vrb(X), SS), !
+  ;S1 = pow(vrb(X), E1), S2 = pow(vrb(X), E2) ->
+      simplify(sub(E1, E2), SS),
+      S3 = pow(vrb(X), SS), !
+  ;
+    S3 = dvd(S1, S2)
+  ).
+operation_expr(pow, S1, S2, S3) :-
+  (
+   S1 = num(0) ->
+      S3 = num(0)
+  ;S1 = num(1) ->
+      S3 = num(1)
+  ;S2 = num(0) ->
+      S3 = num(1)
+  ;S2 = num(1) ->
+      S3 = S1
+  ;S1 = pow(vrb(X), E1)  ->
+      simplify(mul(E1, S2), SS),
+      S3 = pow(vrb(X), SS), !
+  ;
+    S3 = pow(S1, S2)
+  ).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Rules for Integration 
@@ -46,7 +152,10 @@ simplify_operation(Expr, Op, Var, R) :-
   simplify(Expr, S),
   (S = num(N) -> 
     R = mul(num(N), Var)
-  ;
+  ; S = vrb(X), Var = vrb(X) ->
+    R = mul(num(0.5), pow(vrb(X), num(2)))
+  ; S = pow(vrb(X), num(N)) ->
+    R = mul(dvd(num(1), num(N)), pow(vrb(X), add(num(N), num(1)))),
     R =.. [Op, S, Var]
   ).
 
@@ -56,8 +165,46 @@ simplify_operation(Expr, Op, Var, R) :-
 simplify_operation(Expr, Op, Var, R) :-
   Op = diff,
   simplify(Expr, S),
-  (S = num(N) -> 
-    R = num(0)
+  (S = num(_), Var = vrb(_) -> 
+    R = num(0), !
+  ; S = vrb(X), Var = vrb(Y),
+    ( X = Y -> R = num(1), !
+    ; 
+      R = num(0), !
+    )
+  ; S = sin(E) ->
+    simplify(diff(E, Var), D),
+    simplify(mul(D, cos(E)), SS),
+    R = SS
+  ; S = cos(E) ->
+    simplify(diff(E, Var), D),
+    simplify(mul(D, sub(num(0), cos(E))), SS),
+    R = SS
+  ; S = add(E1, E2) ->
+    simplify(diff(E1, Var), S1),
+    simplify(diff(E2, Var), S2),
+    simplify(add(S1, S2), SS),
+    R = SS
+  ; S = sub(E1, E2) -> 
+    simplify(diff(E1, Var), S1),
+    simplify(diff(E2, Var), S2),
+    simplify(sub(S1, S2), SS),
+    R = SS
+  ; S = mul(E1, E2) ->
+    simplify(diff(E1, Var), D1),
+    simplify(diff(E2, Var), D2),
+    simplify(add(mul(D1, E2), mul(E1, D2)), SS),
+    R = SS
+  ; S = dvd(E1, E2) ->
+    simplify(diff(E1, Var), D1),
+    simplify(diff(E2, Var), D2),
+    simplify(dvd(sub(mul(E2, D1), mul(E1, D2)), mul(E2, E2)), SS),
+    % IDEA: why mul(E2, E2), not pow(E2, num(2))
+    R = SS
+  ; S = pow(E1, num(N)) ->
+  % ALARM: there maybe some proble with base is constant
+    simplify(mul(num(N), pow(E1, sub(num(N), num(1)))), SS),
+    R = SS
   ;
     R =.. [Op, S, Var]
   ).
--- tests/simp_ts.pl
+++ tests/simp_ts.pl
@@ -1,5 +1,21 @@
 :- use_module('../src/simp.pl').
 
+
+% :- begin_tests(contradicions). 
+
+% test(zero_contradiction) :-
+%     simplify(pow(num(0), pow(num(0), num(2))), num(0)).
+
+% test(divide_by_zero_edge_cases) :-
+%     simplify(dvd(num(5), num(0)), _),
+%     simplify(dvd(vrb(x), num(0)), _),
+%     simplify(dvd(pow(vrb(x), num(2)), num(0)), _).
+
+% test(imaginary numbers) :- 
+%     simplify(pow(num(-1), dvd(num(1), num(2))), _),
+
+% :- end_tests(contradicions). 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%% Test for Simplification without integration and Differentiation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -20,10 +36,61 @@ test(simple_add_sub_mul_dvd) :-
     simplify(mul(num(2), num(3)), num(6)),
     simplify(dvd(num(6), num(2)), num(3)).
 
+test(zero) :-
+    simplify(add(num(0), num(5)), num(5)),
+    simplify(add(num(5), num(0)), num(5)),
+    simplify(sub(num(5), num(0)), num(5)),
+    simplify(mul(num(0), num(5)), num(0)),
+    simplify(mul(num(5), num(0)), num(0)),
+    simplify(dvd(num(0), num(5)), num(0)),
+    simplify(pow(num(0), num(5)), num(0)),
+    simplify(pow(num(5), num(0)), num(1)),
+    simplify(add(num(0), vrb(x)), vrb(x)),
+    simplify(add(vrb(x), num(0)), vrb(x)),
+    simplify(sub(vrb(x), num(0)), vrb(x)),
+    simplify(mul(num(0), vrb(x)), num(0)),
+    simplify(mul(vrb(x), num(0)), num(0)),
+    simplify(dvd(num(0), vrb(x)), num(0)),
+    simplify(pow(num(0), vrb(x)), num(0)),
+    simplify(pow(vrb(x), num(0)), num(1)).
+
+test(zero_complex_expr) :-
+    simplify(add(mul(num(0), vrb(x)), mul(num(0), vrb(y))), num(0)),
+    simplify(add(mul(num(0), pow(vrb(x), num(2))), mul(num(0), pow(vrb(y), num(2)))), num(0)),
+    simplify(sub(mul(num(0), vrb(x)), mul(num(0), vrb(y))), num(0)),
+    simplify(sub(mul(num(0), pow(vrb(x), num(2))), mul(num(0), pow(vrb(y), num(2)))), num(0)),
+    simplify(mul(num(0), add(vrb(x), vrb(y))), num(0)),
+    simplify(mul(num(0), sub(vrb(x), vrb(y))), num(0)),
+    simplify(mul(num(0), pow(vrb(x), num(2))), num(0)),
+    simplify(mul(num(0), pow(vrb(x), vrb(y))), num(0)).
+
+test(zero_pow_edge_cases) :-
+    simplify(pow(num(1), pow(num(0), num(2))), num(1)),
+    simplify(pow(vrb(x), pow(num(0), num(2))), num(1)),
+    simplify(pow(num(1), pow(num(0), vrb(x))), num(1)),
+    simplify(pow(vrb(x), pow(num(0), vrb(x))), num(1)).
+
+test(invalid_powers) :-
+    simplify(pow(vrb(x), vrb(y)), _),
+    simplify(pow(pow(vrb(x), num(2)), vrb(y)), _).
+
+test(nested_zero_edge_cases) :-
+    simplify(add(mul(num(0), add(vrb(x), vrb(y))), num(5)), num(5)),
+    simplify(mul(num(0), mul(vrb(x), vrb(y))), num(0)),
+    simplify(sub(mul(num(0), sub(vrb(x), vrb(y))), num(6)), num(-6)),
+    simplify(add(num(0), mul(num(0), vrb(x))), num(0)).
+
 test(simle_pow) :-
     simplify(pow(num(2), num(2)), num(4)),
     simplify(pow(num(1.2), num(3.1)), T),
-    T = num(1.7597941219820576).
+    T = num(1.7597941219820576),
+    simplify(pow(vrb(x), num(0)), num(1)),
+    simplify(pow(vrb(x), num(1)), vrb(x)),
+    simplify(pow(num(0), vrb(x)), num(0)),
+    simplify(pow(num(1), vrb(x)), num(1)),
+    simplify(pow(num(-1), num(2)), num(1)),
+    simplify(pow(num(-1), num(3)), num(-1)),
+    simplify(pow(pow(vrb(x), num(2)), num(3)), pow(vrb(x), num(6))).
 
 test(add_variable) :-
     simplify(dvd(num(6), vrb(x)), T1),
@@ -34,7 +101,7 @@ test(add_variable) :-
     simplify(dvd(vrb(h1), num(2)), dvd(vrb(h1), num(2))),
     simplify(sub(vrb(k), num(2)), sub(vrb(k), num(2))).
 
-  test(multi_variable) :-
+test(multi_variable) :-
     simplify(add(vrb(k), vrb(x)), add(vrb(k), vrb(x))),
     simplify(add(vrb(x), add(num(2), vrb(p))), T),
     T = add(vrb(x), add(num(2), vrb(p))).
@@ -48,18 +115,18 @@ test(nested_complex) :-
     T = num(15).
 
 test(mul_with_zero) :-
-    simplify(mul(num(0), x), num(0)).
+    simplify(mul(num(0), vrb(x)), num(0)).
 
 % Test differentiation simplification
 test(diff_simple) :-
-    simplify(diff(x, x), R),
+    simplify(diff(vrb(x), vrb(x)), R),
     R = num(1),
     !.
 
 % Test integration simplification
 test(integ_simple) :-
-    simplify(integ(num(5), x), R),
-    R = mul(num(5), x),
+    simplify(integ(num(5), vrb(x)), R),
+    R = mul(num(5), vrb(x)),
     !.
 
 :- end_tests(simplify).
@@ -67,55 +134,231 @@ test(integ_simple) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%% Test for Integration 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- begin_tests(integration).
+% :- begin_tests(integration).
+
+% test(constant) :-
+%     simplify(integ(num(3), vrb(x)), R),
+%     R = mul(num(3), vrb(x)).
+
+% test(variable) :-
+%     simplify(integ(vrb(x), vrb(x)), R),
+%     R = mul(num(0.5), pow(vrb(x), num(2))).
+
+% test(power) :-
+%     simplify(integ(pow(vrb(x), num(3)), vrb(x)), R),
+%     R = mul(num(0.25), pow(vrb(x), num(4))).
+
+% test(exponential) :-
+%     simplify(integ(exp(vrb(x)), vrb(x)), R),
+%     R = exp(vrb(x)).
+
+% test(logarithmic) :-
+%     simplify(integ(ln(vrb(x)), vrb(x)), R),
+%     R = sub(mul(vrb(x), ln(vrb(x))), vrb(x)).
+
+% test(constant_times_function) :-
+%     simplify(integ(mul(num(2), pow(vrb(x), num(2))), vrb(x)), R),
+%     R = mul(num(2), mul(num(1/3), pow(vrb(x), num(3)))).
+
+% test(sum) :-
+%     simplify(integ(add(pow(vrb(x), num(2)), pow(vrb(x), num(3))), vrb(x)), R),
+%     R = add(mul(num(1/3), pow(vrb(x), num(3))), mul(num(1/4), pow(vrb(x), num(4)))).
+
+% test(difference) :-
+%     simplify(integ(sub(pow(vrb(x), num(3)), pow(vrb(x), num(2))), vrb(x)), R),
+%     R = sub(mul(num(1/4), pow(vrb(x), num(4))), mul(num(1/3), pow(vrb(x), num(3)))).
+
+% % Additional tests
+% test(multiple_terms) :-
+%     simplify(integ(add(mul(num(3), vrb(x)), mul(num(4), pow(vrb(x), num(2)))), vrb(x)), R),
+%     R = add(mul(num(3/2), pow(vrb(x), num(2))), mul(num(4/3), pow(vrb(x), num(3)))).
+
+% test(sin) :-
+%     simplify(integ(sin(vrb(x)), vrb(x)), R),
+%     R = sub(num(0), cos(vrb(x))).
+
+% test(cos) :-
+%     simplify(integ(cos(vrb(x)), vrb(x)), R),
+%     R = sin(vrb(x)).
+
+% :- end_tests(integration).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%% Test for Differentiation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- begin_tests(differentiation).
+
+test(diff_constant) :-
+    simplify(diff(num(3), vrb(x)), R),
+    R = num(0).
+
+test(diff_variable) :-
+    simplify(diff(vrb(x), vrb(x)), R),
+    R = num(1).
+
+test(diff_add_rule) :-
+    simplify(diff(add(num(8), vrb(x)), vrb(x)), R),
+    R = num(1).
+
+test(diff_mul_rule) :-
+    simplify(diff(mul(num(8), vrb(x)), vrb(x)), R),
+    R = num(8).
 
 test(constant) :-
-    simplify(integ(num(3), vrb(x)), R),
-    R = mul(num(3), vrb(x)).
+    simplify(diff(num(5), vrb(x)), num(0)),
+    simplify(diff(num(-5), vrb(x)), num(0)),
+    simplify(diff(num(0), vrb(x)), num(0)).
 
-test(variable) :-
-    simplify(integ(vrb(x), vrb(x)), R),
-    R = mul(num(0.5), pow(vrb(x), num(2))).
+test(linear) :-
+    simplify(diff(vrb(x), vrb(x)), num(1)),
+    simplify(diff(mul(num(3), vrb(x)), vrb(x)), num(3)),
+    simplify(diff(mul(num(-3), vrb(x)), vrb(x)), num(-3)),
+    simplify(diff(mul(num(0), vrb(x)), vrb(x)), num(0)).
 
 test(power) :-
-    simplify(integ(pow(vrb(x), num(3)), vrb(x)), R),
-    R = mul(num(0.25), pow(vrb(x), num(4))).
+    simplify(diff(pow(vrb(x), num(2)), vrb(x)), mul(num(2), vrb(x))),
+    simplify(diff(pow(vrb(x), num(3)), vrb(x)), mul(num(3), pow(vrb(x), num(2)))),
+    simplify(diff(pow(vrb(x), num(0)), vrb(x)), num(0)).
 
-test(exponential) :-
-    simplify(integ(exp(vrb(x)), vrb(x)), R),
+test(product) :-
+    simplify(diff(mul(vrb(x), vrb(y)), vrb(x)), vrb(y)),
+    simplify(diff(mul(vrb(x), vrb(y)), vrb(y)), vrb(x)),
+    simplify(diff(mul(num(2), mul(vrb(x), vrb(y))), vrb(x)), mul(num(2), vrb(y))).
+
+test(quotient) :-
+    simplify(diff(dvd(vrb(x), vrb(y)), vrb(x)), pow(vrb(y), num(-1))),
+    simplify(diff(dvd(vrb(y), vrb(x)), vrb(x)), dvd(num(-1), pow(vrb(x), num(2)))),
+    simplify(diff(dvd(num(2), mul(vrb(x), vrb(y))), vrb(x)), dvd(num(2), mul(vrb(y), pow(vrb(x), num(2))))).
+
+test(chain) :-
+    simplify(diff(sin(vrb(x)), vrb(x)), cos(vrb(x))),
+    simplify(diff(cos(vrb(x)), vrb(x)), mul(num(-1), sin(vrb(x)))),
+    simplify(diff(exp(vrb(x)), vrb(x)), exp(vrb(x))).
+
+test(chain_complex) :-
+    simplify(diff(sin(mul(num(2), vrb(x))), vrb(x)), mul(num(2), cos(mul(num(2), vrb(x))))),
+    simplify(diff(exp(add(vrb(x), num(2))), vrb(x)), exp(add(vrb(x), num(2)))),
+    simplify(diff(cos(sub(vrb(x), vrb(y))), vrb(x)), mul(num(-1), sin(sub(vrb(x), vrb(y))))),
+    simplify(diff(cos(sub(vrb(x), vrb(y))), vrb(y)), sin(sub(vrb(x), vrb(y)))).
+
+test(chain_product) :-
+    simplify(diff(mul(exp(vrb(x)), sin(vrb(x))), vrb(x)), add(mul(exp(vrb(x)), sin(vrb(x))), mul(exp(vrb(x)), cos(vrb(x))))),
+    simplify(diff(mul(cos(vrb(x)), pow(vrb(x), num(3))), vrb(x)), add(mul(mul(num(-1), sin(vrb(x))), pow(vrb(x), num(3))), mul(num(3), mul(cos(vrb(x)), pow(vrb(x), num(2)))))).
+
+test(diff_power) :-
+    simplify(diff(pow(vrb(x), num(3)), vrb(x)), R),
+    R = mul(num(3), pow(vrb(x), num(2))).
+
+test(diff_exponential) :-
+    simplify(diff(exp(vrb(x)), vrb(x)), R),
     R = exp(vrb(x)).
 
-test(logarithmic) :-
-    simplify(integ(ln(vrb(x)), vrb(x)), R),
-    R = sub(mul(vrb(x), ln(vrb(x))), vrb(x)).
+test(diff_logarithmic) :-
+    simplify(diff(ln(vrb(x)), vrb(x)), R),
+    R = dvd(num(1), vrb(x)).
 
-test(constant_times_function) :-
-    simplify(integ(mul(num(2), pow(vrb(x), num(2))), vrb(x)), R),
-    R = mul(num(2), mul(num(1/3), pow(vrb(x), num(3)))).
+test(diff_sin) :-
+    simplify(diff(sin(vrb(x)), vrb(x)), R),
+    R = cos(vrb(x)).
 
-test(sum) :-
-    simplify(integ(add(pow(vrb(x), num(2)), pow(vrb(x), num(3))), vrb(x)), R),
-    R = add(mul(num(1/3), pow(vrb(x), num(3))), mul(num(1/4), pow(vrb(x), num(4)))).
+test(diff_cos) :-
+    simplify(diff(cos(vrb(x)), vrb(x)), R),
+    R = mul(num(-1), sin(vrb(x))).
 
-test(difference) :-
-    simplify(integ(sub(pow(vrb(x), num(3)), pow(vrb(x), num(2))), vrb(x)), R),
-    R = sub(mul(num(1/4), pow(vrb(x), num(4))), mul(num(1/3), pow(vrb(x), num(3)))).
+test(diff_tan) :-
+    simplify(diff(tan(vrb(x)), vrb(x)), R),
+    R = pow(sec(vrb(x)), num(2)).
 
-% Additional tests
-test(multiple_terms) :-
-    simplify(integ(add(mul(num(3), vrb(x)), mul(num(4), pow(vrb(x), num(2)))), vrb(x)), R),
-    R = add(mul(num(3/2), pow(vrb(x), num(2))), mul(num(4/3), pow(vrb(x), num(3)))).
+test(diff_product_rule) :-
+    simplify(diff(mul(vrb(x), exp(vrb(x))), vrb(x)), R),
+    R = add(exp(vrb(x)), mul(vrb(x), exp(vrb(x)))).
 
-test(sin) :-
-    simplify(integ(sin(vrb(x)), vrb(x)), R),
-    R = sub(num(0), cos(vrb(x))).
+test(diff_quotient_rule) :-
+    simplify(diff(dvd(vrb(x), exp(vrb(x))), vrb(x)), R),
+    R = dvd(sub(exp(vrb(x)), vrb(x)), pow(exp(vrb(x)), num(2))).
 
-test(cos) :-
-    simplify(integ(cos(vrb(x)), vrb(x)), R),
-    R = sin(vrb(x)).
+test(diff_chain_rule) :-
+    simplify(diff(exp(sin(vrb(x))), vrb(x)), R),
+    R = mul(cos(vrb(x)), exp(sin(vrb(x)))).
 
-:- end_tests(integration).
+test(diff_second_derivative) :-
+    simplify(diff(diff(pow(vrb(x), num(3)), vrb(x)), vrb(x)), R),
+    R = mul(num(6), vrb(x)).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%% Test for Integration 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+test(diff_third_derivative) :-
+    simplify(diff(diff(diff(pow(vrb(x), num(3)), vrb(x)), vrb(x)), vrb(x)), R),
+    R = num(6).
+
+test(diff_complex_expr_1) :-
+    simplify(diff(sin(mul(exp(vrb(x)), cos(vrb(x)))), vrb(x)), R),
+    R = mul(exp(vrb(x)), mul(cos(vrb(x)), mul(cos(mul(exp(vrb(x)), cos(vrb(x)))), add(mul(sin(vrb(x)), exp(vrb(x))), mul(num(-1), sin(vrb(x))))))).
+
+test(diff_complex_expr_2) :-
+    simplify(diff(pow(ln(mul(vrb(x), exp(vrb(x)))), num(2)), vrb(x)), R),
+    R = mul(dvd(num(2), vrb(x)), ln(mul(vrb(x), exp(vrb(x))))).
+
+test(diff_complex_expr_3) :-
+    simplify(diff(diff(diff(diff(mul(vrb(x), ln(vrb(x))), vrb(x)), vrb(x)), vrb(x)), vrb(x)), R),
+    R = dvd(num(6), pow(vrb(x), num(4))).
+
+test(diff_complex_expr_4) :-
+  simplify(diff(add(pow(vrb(x), num(3)), mul(num(2), pow(vrb(x), num(2))), mul(num(3), vrb(x)), num(4)), vrb(x)), R),
+  R = add(mul(num(3), pow(vrb(x), num(2))), mul(num(4), vrb(x)), num(3)).
+
+test(diff_complex_expr_5) :-
+  simplify(diff(mul(sin(vrb(x)), add(exp(vrb(x)), num(1))), vrb(x)), R),
+  R = add(mul(cos(vrb(x)), add(exp(vrb(x)), num(1))), mul(sin(vrb(x)), exp(vrb(x)))).
+
+test(diff_complex_expr_6) :-
+  simplify(diff(dvd(mul(pow(vrb(x), num(3)), exp(vrb(x))), add(vrb(x), num(1))), vrb(x)), R),
+  R = dvd(sub(mul(exp(vrb(x)), add(mul(num(4), pow(vrb(x), num(2))), mul(num(3), vrb(x)))), mul(vrb(x), mul(pow(vrb(x), num(3)), exp(vrb(x))))), pow(add(vrb(x), num(1)), num(2))).
+
+test(diff_complex_expr_7) :-
+  simplify(diff(mul(cos(vrb(x)), sin(add(vrb(x), exp(vrb(x))))), vrb(x)), R),
+  R = add(mul(num(-1), mul(sin(vrb(x)), sin(add(vrb(x), exp(vrb(x)))))), mul(cos(vrb(x)), mul(cos(add(vrb(x), exp(vrb(x)))), exp(vrb(x))))).
+
+test(diff_complex_expr_8) :-
+  simplify(diff(pow(exp(pow(vrb(x), num(2))), num(3)), vrb(x)), R),
+  R = mul(num(6), mul(exp(mul(pow(vrb(x), num(2)), num(3))), vrb(x))).
+
+test(diff_complex_expr_9) :-
+  simplify(diff(ln(mul(vrb(x), add(vrb(x), num(1)))), vrb(x)), R),
+  R = dvd(add(num(1), vrb(x)), mul(vrb(x), add(vrb(x), num(1)))).
+
+test(diff_complex_expr_10) :-
+  simplify(diff(pow(cos(add(mul(vrb(x), num(2)), num(3))), num(2)), vrb(x)), R),
+  R = mul(num(-4), mul(vrb(x), sin(add(mul(vrb(x), num(2)), num(3))))).
+
+test(diff_complex_expr_11) :-
+  simplify(diff(pow(sin(pow(vrb(x), num(3))), num(4)), vrb(x)), R),
+  R = mul(num(12), mul(pow(vrb(x), num(2)), mul(pow(sin(pow(vrb(x), num(3))), num(3)), cos(pow(vrb(x), num(3)))))).
+
+test(diff_complex_expr_12) :-
+  simplify(diff(exp(mul(pow(vrb(x), num(2)), num(3))), vrb(x)), R),
+  R = mul(num(6), mul(vrb(x), exp(mul(pow(vrb(x), num(2)), num(3))))).
+
+test(diff_complex_expr_13) :-
+  simplify(diff(add(pow(vrb(x), num(3)), mul(pow(vrb(x), num(2)), num(2)), mul(vrb(x), num(3)), num(4)), vrb(x)), R),
+  R = add(mul(num(3), pow(vrb(x), num(2))), mul(num(4), vrb(x)), num(3)).
+
+test(diff_complex_expr_14) :-
+  simplify(diff(add(exp(vrb(x)), mul(cos(vrb(x)), sin(vrb(x)))), vrb(x)), R),
+  R = add(exp(vrb(x)), sub(mul(num(-1), sin(vrb(x))), mul(cos(vrb(x)), cos(vrb(x))))).
+
+test(diff_complex_expr_15) :-
+  simplify(diff(mul(ln(add(vrb(x), num(1))), exp(vrb(x))), vrb(x)), R),
+  R = add(mul(exp(vrb(x)), ln(add(vrb(x), num(1)))), mul(exp(vrb(x)), dvd(num(1), add(vrb(x), num(1))))).
+
+test(diff_complex_expr_16) :-
+  simplify(diff(sub(pow(vrb(x), num(4)), mul(num(4), pow(vrb(x), num(2)))), vrb(x)), R),
+  R = sub(mul(num(4), pow(vrb(x), num(3))), mul(num(8), vrb(x))).
+
+test(diff_complex_expr_17) :-
+  simplify(diff(dvd(mul(num(2), exp(vrb(x))), add(vrb(x), num(1))), vrb(x)), R),
+  R = dvd(sub(mul(num(2), exp(vrb(x))), mul(num(2), mul(exp(vrb(x)), add(vrb(x), num(1))))), pow(add(vrb(x), num(1)), num(2))).
+
+test(diff_complex_expr_18) :-
+  simplify(diff(add(sin(vrb(x)), pow(cos(vrb(x)), num(3))), vrb(x)), R),
+  R = sub(mul(num(3), mul(pow(cos(vrb(x)), num(2)), num(-1), sin(vrb(x)))), cos(vrb(x))).
+
+:- end_tests(differentiation).
